57a74b17ccb9bcea9adf3d8bd026a85c4e481d93
Tiezheng Song <359011896@qq.com>
2018-11-12 16:46:23 +0800

    Add files via upload

--- Datalog2Sql.py
+++ Datalog2Sql.py
@@ -54,11 +54,51 @@ def parseDatalog2Tree(clauses):
         n.add_child(literal)
     return n
 
-def translateDatalog2Sql(n):
+def mapFactSchema(factList, inputSchemas):
+    '''
+    Description: make a dictionary to contain the fact schama and corresponds fact
+    type list: factList
+    type list: inputSchema
+    '''
+    results = {}
+    for fact in factList:
+        pred = getPredPerTerm(fact)
+        for schema in inputSchemas:
+            if schema.find(pred) != -1:
+                for i in range(len(getTermsInParens(schema))):
+                    key = pred + "." + getTermsInParens(schema)[i]
+                    if key in results:
+                        results[key].append(getTermsInParens(fact)[i])
+                    else:
+                        results[key] = [getTermsInParens(fact)[i]]
+    return results
+
+
+def getSchemaDict(inputSchemas):
+    '''
+    Description: Create a dictionary for schemas
+    type list: inputSchemas
+    rtype dictionary
+    '''
+    schemaDict = {}
+    for schema in inputSchemas:
+        pred = getPredPerTerm(schema)
+        if pred in schemaDict:
+            schemaDict[pred] += getTermsInParens(schema)
+        else:
+            schemaDict[pred] = getTermsInParens(schema)
+    return schemaDict
+
+
+def translateDatalog2Sql(n, factList, inputSchemas):
     '''
     type Node: n
+    type list: inputSchemas
+    type list: factList
     rtype sqlQuery: string
     '''
+    schemaDict = getSchemaDict(inputSchemas)
+    factSchemaDict = mapFactSchema(factList, inputSchemas)
     sqlQuery = ''
     sqlQueryFrom = 'From '
     sqlQueryWhere = 'Where '
@@ -73,25 +113,37 @@ def translateDatalog2Sql(n):
         tab = getPredPerTerm(children)
         sqlQueryFrom += tab + ', '
         terms = getTermsInParens(children)
-        for term in terms:
-            termCol = tab + '.' + term
-            if term not in duplicateTerms:
-                duplicateTerms[term] = termCol
+        # Update where clause
+        for termPos in range(len(terms)):
+            termCol = tab + '.' + terms[termPos]
+            if termCol not in factSchemaDict:
+                ### Need to edit
+                sqlQueryWhere += tab + '.' + schemaDict[tab][termPos] + '=' + terms[termPos]  +" and "
+            if terms[termPos] not in duplicateTerms:
+                duplicateTerms[terms[termPos]] = termCol
             else:
-                sqlQueryWhere += duplicateTerms[term] + "=" + termCol + ", "
+                sqlQueryWhere += duplicateTerms[terms[termPos]] + "=" + termCol + " and "
+        # Update select clause
+        for term in terms:
             if term in headTerms and i <= 2:
                 sqlQuerySelect += termCol + ", "
                 i += 1
 
-    sqlQuery += sqlQueryView + "\n" + sqlQueryFrom.strip(', ') + "\n" + sqlQueryWhere.strip(', ') + "\n" + sqlQuerySelect.strip(', ') + ')'
+    sqlQuery += sqlQueryView + "\n" + sqlQueryFrom.strip(', ') + "\n" + sqlQueryWhere.strip(' and ') + "\n" + sqlQuerySelect.strip(', ') + ')'
     return sqlQuery
+
+def mainCode(datalog, factList, inputSchemas):
+    clauses = getClauses(datalog)
+    n = parseDatalog2Tree(clauses)
+    sqlQuery = translateDatalog2Sql(n, factList, inputSchemas)
+    print(sqlQuery)
 
 
 # Input Datalog query
 # Note: 1. each facts are seperated by ', '
-#       2. head and body are seperated by '：-‘
-datalog = "r1(A,B):-fact1(A,X), fact2(X,Y), fact3(Y,B). "
-clauses = getClauses(datalog)
-n = parseDatalog2Tree(clauses)
-sqlQuery = translateDatalog2Sql(n)
-print(sqlQuery)
+#       2. head and body are seperated by '：-'
+#       3. input: datalog query, factlist, schemalist
+datalog =  'r1(a,b):-fact1(x,a), fact2(20,y), fact3(y,b).'
+factList =['fact1(2,1)','fact1(2,3)','fact1(2,4)','fact2(20,10)','fact2(20,30)','fact2(20,40)', 'fact3(22,21)','fact3(22,23)','fact3(22,24)']
+inputSchemas = ['fact1(x,a)', 'fact2(a,y)', 'fact3(y,b)']
+mainCode(datalog,factList,inputSchemas)
